2689a4134aa56ce9d2c5b9039e910c688e43706a
cheptsova <116488910+cheptsova@users.noreply.github.com>
2023-01-17 19:16:04 +0000

    Deploying to gh-pages from @ cheptsova/banda-ice-cream@aa10d17aa024954f802658e3fabad7625ad4c517 🚀

---
+++ index.e9fb0bad.js
@@ -0,0 +1,2 @@
+$(".one-time").slick({dots:!0,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0,arrows:!1,autoplay:!0}),$(".gallery-slaid").slick({dots:!1,infinite:!0,speed:200,slidesToShow:1,adaptiveHeight:!0,arrows:!1,autoplay:!0});
+//# sourceMappingURL=index.e9fb0bad.js.map
